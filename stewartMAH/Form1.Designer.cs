47580aa8997ec5ebe939dcc24ecd7d06bbadb2f9
kxnst <stadnick0673595557@gmail.com>
2019-04-30 13:36:55 +0300

    Commit

---
+++ stewartMAH/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace stewartMAH
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Location = new System.Drawing.Point(602, 365);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Знайти корені\r\n";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(601, 163);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(37, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(601, 212);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(37, 20);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(689, 163);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(37, 20);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(689, 212);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(37, 20);
+            this.textBox4.TabIndex = 4;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            this.textBox4.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(689, 266);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(37, 20);
+            this.textBox5.TabIndex = 5;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            this.textBox5.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(601, 266);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(37, 20);
+            this.textBox6.TabIndex = 6;
+            this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            this.textBox6.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(52, 38);
+            this.textBox7.Multiline = true;
+            this.textBox7.Name = "textBox7";
+            this.textBox7.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox7.Size = new System.Drawing.Size(403, 352);
+            this.textBox7.TabIndex = 8;
+            this.textBox7.TextChanged += new System.EventHandler(this.textBox7_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(644, 166);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "<=a<=";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(644, 215);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "<=b<=";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(644, 269);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "<=c<=";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(52, 358);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 19;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(581, 38);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(148, 56);
+            this.pictureBox1.TabIndex = 20;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Cursor = System.Windows.Forms.Cursors.Help;
+            this.label4.Location = new System.Drawing.Point(12, 399);
+            this.label4.Margin = new System.Windows.Forms.Padding(3);
+            this.label4.Name = "label4";
+            this.label4.Padding = new System.Windows.Forms.Padding(2);
+            this.label4.Size = new System.Drawing.Size(105, 19);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "Як користуватися";
+            this.label4.Click += new System.EventHandler(this.Інструкція_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(305, 401);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(150, 17);
+            this.checkBox1.TabIndex = 22;
+            this.checkBox1.Text = "Розв\'язок у трикутниках";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 430);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Рівняння Стюарта";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ stewartMAH/Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Windows.Forms;
+//&& b+p>y && b+y>p && y+p>b && p+c>x && c+x>p && p+x>c && b+c>a && c+b>a && c+a>b
+namespace stewartMAH
+{
+    public partial class Form1 : Form
+    {
+        public void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if (!Char.IsDigit(number) && number != 8)
+            {
+                e.Handled = true;
+            }
+        }
+        public void pictureBox1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Створено учнем 41 групи БНВО\"Ліцей - МАН\"\nСтадником Костянтином Петровичем\n Учасником третьго етапу конкурсу - захисту наукових робіт МАН\n Відділення: математика\nСекція: математика ");
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            istriangle = !istriangle;
+        }
+
+        public void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        public void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        public void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        public void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        public void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        public void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        public double a, b, c,  x, y, am, bm, cm, xm, ym;
+        double p;
+        double an; double bn; double cn; double ann; double bnn; double cnn; bool istriangle;
+        bool LastCheck(double n)
+        {
+            if (Math.Sqrt(n) == Math.Round(Math.Sqrt(n)))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void Form1_Load(object sender, EventArgs e)
+        {
+            istriangle = false;
+        }
+        public void textBox7_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+        private void Інструкція_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("1. Введіть межі для a, b, c\r\n2. Натисніть \"Знайти корені\"\r\n");
+        }
+
+        public void Stewart(double ak, double bk, double ck, double akk, double bkk, double ckk)
+        {
+            for(a = ak; a <= akk; a++)
+            {
+                for(c = ck; c<=ckk; c++)
+                {
+                    for(y =3; y<=50; y++)
+                    {
+                        for(b = bk; b<bkk; b++)
+                        {
+                            for(x = 3; x<50; x++)
+                            {
+                                
+                                p = ((b * b * x) / a + (c * c * y) / a) - (x * y);
+                              
+                                if (p % 1 != 0 )
+                                {
+                                    continue;
+                                }
+                                
+                                if((b*b*x + c * c * y) / a % 1 != 0)
+                                {
+                                    continue;
+                                }
+                                else if (Math.Sqrt(p)!=-1 && Math.Sqrt(p)%1==0 )
+                                {
+                                   textBox7.Text=textBox7.Text + $"a = {a}, b = {b}, c = {c}, x = {x}, y = {y}, p = {Math.Sqrt(p)}" + "\r\n\r\n";
+                                }
+                                else
+                                {
+                                    continue;
+                                }                   
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        public void StewartTriangle(double ak, double bk, double ck, double akk, double bkk, double ckk)
+        {
+            for (a = ak; a <= akk; a++)
+            {
+                for (c = ck; c <= ckk; c++)
+                {
+                    for (y = 3; y <= 50; y++)
+                    {
+                        for (b = bk; b < bkk; b++)
+                        {
+                            for (x = 3; x < 50; x++)
+                            {
+
+                                p = ((b * b * x) / a + (c * c * y) / a) - (x * y);
+
+                                if (p % 1 != 0)
+                                {
+                                    continue;
+                                }
+                               
+                                if ((b * b * x + c * c * y) / a % 1 != 0)
+                                {
+                                    continue;
+                                }
+                                else if (Math.Sqrt(p) != -1 && Math.Sqrt(p) % 1 == 0 && b + p > y && b + y > p && y + p > b && p + c > x && c + x > p && p + x > c && b + c > a && c + b > a && c + a > b && x + y == c )
+                                {
+
+                                    textBox7.Text = textBox7.Text + $"a = {a}, b = {b}, c = {c}, x = {x}, y = {y}, p = {Math.Sqrt(p)}" + "\r\n\r\n";
+
+                                }
+                                else
+                                {
+                                    continue;
+                                }
+
+
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public void IsEmpty()
+        {
+            if (textBox1.Text=="")
+            {
+                textBox1.Text = "3";
+            }
+            if (textBox3.Text=="")
+            {
+                textBox3.Text = "6";
+            }
+            if (textBox2.Text=="")
+            {
+                textBox2.Text = "3";
+            }
+            if (textBox4.Text=="")
+            {
+                textBox4.Text = "6";
+            }
+            if (textBox6.Text == "")
+            {
+                textBox6.Text = "3";
+            }
+            if (textBox5.Text=="")
+            {
+                textBox5.Text = "6";
+            }
+        }
+
+        
+        public int IsSquare(double k)
+        {
+            if (k < 0)
+            {
+                return -1;
+            }
+            if (Math.Sqrt(k)%1==0 && Math.Round(k)==k && k%1==0)
+            {
+                return Convert.ToInt32(Math.Sqrt(k));
+            }
+            else
+            {
+                return -1;
+            }
+        }
+        public void Checkerror()
+        {
+            if(textBox1.Text == null||textBox1.Text=="")
+            {
+                textBox7.Text = ("error");
+            }
+            textBox7.Text += ($"{textBox1.Text}, {textBox3.Text}, {textBox2.Text}, {textBox4.Text}, {textBox6.Text}, {textBox5.Text}");
+        }
+        public void button1_Click(object sender, EventArgs e)
+        {
+            textBox7.Text = null;
+            IsEmpty();
+          //  Checkerror();
+            an = Convert.ToDouble(textBox1.Text);
+            bnn = Convert.ToDouble(textBox4.Text);
+            bn = Convert.ToDouble(textBox2.Text);
+            ann = Convert.ToDouble(textBox3.Text);
+            cnn = Convert.ToDouble(textBox5.Text);
+            cn = Convert.ToDouble(textBox6.Text);
+            if (istriangle)
+                StewartTriangle(an, bn, cn, ann, bnn, cnn);
+            else if (!istriangle)
+                Stewart(an, bn, cn, ann, bnn, cnn);
+           
+        }   
+    }
+}
